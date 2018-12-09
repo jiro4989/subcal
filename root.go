7f6dd3d171c5b52eaa77fc824dce9b05dffb81df
jiro4989 <jiroron666@gmail.com>
2018-12-09 17:58:34 +0900

    :recycle:コードを関数化

--- root.go
+++ root.go
@@ -2,12 +2,54 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net"
 	"strings"
 
 	"github.com/spf13/cobra"
 )
+
+type IP struct {
+	IPAddress string
+	CIDR      int
+	Bin       string
+	Mask      string
+}
+
+func ParseCIDR(s string) (IP, error) {
+	ipv4Addr, ipv4Net, err := net.ParseCIDR(s)
+	if err != nil {
+		return IP{}, err
+	}
+
+	var ip IP
+	ip.IPAddress = ipv4Addr.String()
+
+	ones, _ := ipv4Net.Mask.Size()
+	ip.CIDR = ones
+
+	l := len(ipv4Addr)
+	ip.Bin = fmt.Sprintf("%08b%08b%08b%08b", ipv4Addr[l-4], ipv4Addr[l-3], ipv4Addr[l-2], ipv4Addr[l-1])
+	ip.Mask = fmt.Sprintf("%08b%08b%08b%08b", ipv4Net.Mask[0], ipv4Net.Mask[1], ipv4Net.Mask[2], ipv4Net.Mask[3])
+
+	return ip, nil
+}
+
+func (ip IP) Format(sep string, colorFlag, ipv4Flag, cidrFlag, binFlag, maskFlag bool) string {
+	var arr []string
+	if ipv4Flag {
+		arr = append(arr, ip.IPAddress)
+	}
+	if cidrFlag {
+		arr = append(arr, fmt.Sprintf("%d", ip.CIDR))
+	}
+	if binFlag {
+		arr = append(arr, ip.Bin)
+	}
+	if maskFlag {
+		arr = append(arr, ip.Mask)
+	}
+	return strings.Join(arr, sep)
+}
 
 var RootCommand = &cobra.Command{
 	Use:   "subcal",
@@ -19,34 +61,22 @@ var RootCommand = &cobra.Command{
 			return
 		}
 		for _, ipcidr := range args {
-			var arr []string
-			ipv4Addr, ipv4Net, err := net.ParseCIDR(ipcidr)
+			ip, err := ParseCIDR(ipcidr)
 			if err != nil {
-				log.Fatal(err)
+				panic(err)
 			}
-			arr = append(arr, ipv4Addr.String())
-
-			ones, _ := ipv4Net.Mask.Size()
-			arr = append(arr, fmt.Sprintf("%d", ones))
-
-			l := len(ipv4Addr)
-			ipbin := fmt.Sprintf("%08b%08b%08b%08b", ipv4Addr[l-4], ipv4Addr[l-3], ipv4Addr[l-2], ipv4Addr[l-1])
-			arr = append(arr, ipbin)
-
-			mask := fmt.Sprintf("%08b%08b%08b%08b", ipv4Net.Mask[0], ipv4Net.Mask[1], ipv4Net.Mask[2], ipv4Net.Mask[3])
-			arr = append(arr, mask)
-
-			joined := strings.Join(arr, "\t")
-			fmt.Println(joined)
+			fmt.Println(ip)
 		}
 	},
 }
 
 func init() {
 	cobra.OnInitialize()
+	RootCommand.Flags().StringP("sep", "s", "\t", "color")
 	RootCommand.Flags().BoolP("color", "c", false, "color")
 	RootCommand.Flags().BoolP("ipv4", "i", false, "color")
 	RootCommand.Flags().BoolP("cidr", "d", false, "color")
 	RootCommand.Flags().BoolP("bin", "b", false, "color")
 	RootCommand.Flags().BoolP("mask", "m", false, "color")
+	RootCommand.Flags().BoolP("noheader", "H", false, "color")
 }
