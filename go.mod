d034b724a418abd74bd0f9c27abd48f038f921c1
jiro4989 <jiroron666@gmail.com>
2019-09-02 13:13:08 +0900

    :sparkles: 移行前と同程度の挙動をするようにした

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/jiro4989/subcal
 go 1.12
 
 require (
+	github.com/docopt/docopt-go v0.0.0-20180111231733-ee0de3bc6815
 	github.com/fatih/color v1.7.0
+	github.com/jiro4989/ponpe v0.0.0-20190831094119-1e12a30bc3f3 // indirect
 	github.com/mattn/go-colorable v0.1.2 // indirect
 	github.com/mattn/go-isatty v0.0.9 // indirect
 	github.com/spf13/cobra v0.0.5
--- main.go
+++ main.go
@@ -2,19 +2,73 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 
-	"github.com/jiro4989/subcal/command"
+	"github.com/docopt/docopt-go"
+	"github.com/jiro4989/subcal/ip"
 )
 
-func init() {
-	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
-}
+type (
+	Config struct {
+		Delimiter string `docopt:"-d,--delimiter"`
+		Color     bool
+		IPv4      bool `docopt:"--ipv4"`
+		CIDR      bool `docopt:"--cidr"`
+		Bin       bool
+		Mask      bool
+		Header    bool
+		IP        []string `docopt:"<ip>"`
+	}
+)
+
+const (
+	doc = `subcal はサブネットマスクを簡単に参照するためのコマンドです。
+
+Usage:
+	saubcal [options] <ip>...
+	saubcal -h | --help
+	saubcal -v | --version
+
+Options:
+	-h --help         このヘルプを出力する。
+	-v --version      バージョン情報を出力する。
+	-d --delimiter=<DELIMITER>   delimiter [default:  ]
+	-C --color        delimiter
+	-i --ipv4         delimiter
+	-c --cidr         delimiter
+	-b --bin          delimiter
+	-m --mask         delimiter
+	-H --header       delimiter`
+)
 
 func main() {
-	if err := command.RootCommand.Execute(); err != nil {
+	os.Exit(Main(os.Args))
+}
+
+func Main(argv []string) int {
+	parser := &docopt.Parser{}
+	args, _ := parser.ParseArgs(doc, argv[1:], Version)
+	config := Config{}
+	err := args.Bind(&config)
+	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
-		os.Exit(-1)
+		return 1
 	}
+
+	if !config.IPv4 && !config.CIDR && !config.Bin && !config.Mask {
+		config.IPv4 = true
+		config.CIDR = true
+		config.Bin = true
+		config.Mask = true
+	}
+
+	for _, ipcidr := range config.IP {
+		ipaddr, err := ip.ParseCIDR(ipcidr)
+		if err != nil {
+			return 1
+		}
+		fmt.Println(ipaddr.Format(config.Delimiter, config.Color, config.IPv4, config.CIDR, config.Bin, config.Mask))
+	}
+
+	return 0
 }
